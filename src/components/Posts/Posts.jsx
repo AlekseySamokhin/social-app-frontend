20ffbd1ed60782d9832ba7fa9fdc3926e53b2c80
Aleksey Samokhin <lel.dude01@gmail.com>
2023-03-10 16:55:40 +0300

    feat: added posts component

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,7 +1,31 @@
+import Post from './component/Post';
 import './posts.scss';
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const posts = [
+    {
+      id: 1,
+      name: 'Jonh Doe',
+      userId: 2,
+      desc: 'jdgjlasjdglasd asdhadsjldashdash dashklasdhkadhsdhas ashda;ls;ladshkldasl;dhlas',
+      img: 'https://64.media.tumblr.com/163cfedc157fa648c1aa5d826b0216b6/b70cfb1cda5450f2-d9/s1280x1920/104de63740b6ee7f5a87eb8c0720abd88d268cbe.jpg',
+    },
+    {
+      id: 2,
+      name: 'Jonh Doe',
+      userId: 3,
+      desc: 'jdgjlasjdglasd asdhadsjldashdash dashklasdhkadhsdhas ashda;ls;ladshkldasl;dhlas',
+      img: 'https://64.media.tumblr.com/163cfedc157fa648c1aa5d826b0216b6/b70cfb1cda5450f2-d9/s1280x1920/104de63740b6ee7f5a87eb8c0720abd88d268cbe.jpg',
+    },
+  ];
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
